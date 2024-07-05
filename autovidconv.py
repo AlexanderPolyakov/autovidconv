127173fedbbe0adef65978d9cf07c417755557a9
Alexander Polyakov <nolokor@gmail.com>
2024-07-04 19:15:32 -0700

    Specify output dir + sleep between scans

--- autovidconv.py
+++ autovidconv.py
@@ -1,12 +1,15 @@
 import ffmpeg
 import argparse
 import os
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dir")
+parser.add_argument("output")
 args = parser.parse_args()
 
 while True:
+    time.sleep(1) # lazily sleep here
     for root, dirs, files in os.walk(args.dir):
         for filename in files:
             
@@ -14,9 +17,9 @@ while True:
                 continue
 
             fullpath = os.path.join(root, filename)
-            basename, ext = os.path.splitext(fullpath)
+            basename, ext = os.path.splitext(filename)
             print(f"processing {fullpath}")
-            ffmpeg.input(fullpath).output(basename + ".mp4").run()
+            ffmpeg.input(fullpath).output(os.path.join(args.output, basename) + ".mp4").run()
             print(f"done")
             os.remove(fullpath)
 
